b48c9b5f4c7006cbb5e686ed7c6f88b66be0d49a
Steven Anderson <sdandersonz@icloud.com>
2023-03-20 08:58:41 -0400

    unoptimize transformer. around 0.02s per iter loop

--- mygpt/model.py
+++ mygpt/model.py
@@ -2,16 +2,24 @@ import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from dataclasses import dataclass
 
-
+@dataclass
+class TransformerConfig:
+    block_size: int = 32
+    vocab_size: int = 116 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 6
+    n_head: int = 4
+    n_embd: int = 32
+    dropout: float = 0.2
 
 class Head (nn.Module):
-    def __init__(self,ctx_size, n_embd, head_size):
+    def __init__(self,config,head_size):
         super().__init__()
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('bias', torch.tril(torch.ones(ctx_size, ctx_size)))
+        self.value = nn.Linear(config.n_embd, head_size, bias=False)
+        self.query = nn.Linear(config.n_embd, head_size, bias=False)
+        self.key = nn.Linear(config.n_embd, head_size, bias=False)
+        self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)))
     def forward(self, x):
         B,T,C = x.shape
         k = self.key(x) # B, T, T
@@ -33,39 +41,39 @@ class Head (nn.Module):
         return out
 
 class MultiheadAttention (nn.Module):
-    def __init__(self, n_head, ctx_size, n_embd, head_size):
+    def __init__(self, config, head_size):
         super().__init__()
         # n_heads must be 4x greater than head_size
-        self.sa_heads = nn.ModuleList([Head(ctx_size, n_embd, head_size) for _ in range(n_head)])
-        self.ffwd = FeedForwardNet(head_size*n_head)
-        self.dropout = nn.Dropout(0.2)
+        self.sa_heads = nn.ModuleList([Head(config, head_size) for _ in range(config.n_head)])
+        self.proj = nn.Linear(config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
 
     def forward(self,x):
         x = torch.cat([h(x) for h in self.sa_heads], dim=-1)
-        x = self.dropout(self.ffwd(x))
+        x = self.dropout(self.proj(x))
         return x       
 
 class FeedForwardNet (nn.Module):
-    def __init__(self, n_embd):
+    def __init__(self, config):
         super().__init__()
         self.ffwd = nn.Sequential(
-            nn.Linear(n_embd, n_embd*4),
-            nn.Linear(n_embd*4, n_embd),
+            nn.Linear(config.n_embd, config.n_embd*4),
+            nn.Linear(config.n_embd*4, config.n_embd),
             nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Dropout(config.dropout)
         )
     def forward(self,x):
         return self.ffwd(x)
 
 class Block(nn.Module):
 
-    def __init__(self, n_embd, n_head, ctx_size):
+    def __init__(self, config):
         super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiheadAttention(n_head=n_head, head_size=head_size, ctx_size=ctx_size, n_embd=n_embd)
-        self.ffwd = FeedForwardNet(n_embd)
-        self.ln1  = nn.LayerNorm(n_embd)
-        self.ln2  = nn.LayerNorm(n_embd)
+        head_size = config.n_embd // config.n_head
+        self.sa = MultiheadAttention(config, head_size)
+        self.ffwd = FeedForwardNet(config)
+        self.ln1  = nn.LayerNorm(config.n_embd)
+        self.ln2  = nn.LayerNorm(config.n_embd)
 
     def forward(self,x):
         x = x + self.sa(self.ln1(x))
@@ -74,14 +82,14 @@ class Block(nn.Module):
         
 
 class Transformer (nn.Module):
-    def __init__(self, vocab_size, ctx_size, n_embd, n_head, n_layer):
+    def __init__(self, config):
         super().__init__()
-        self.ctx_size = ctx_size
-        self.toks_embd = nn.Embedding(vocab_size, n_embd)
-        self.pos_embd = nn.Embedding(ctx_size, n_embd)
-        self.lm_head = nn.Linear(n_embd, vocab_size)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head, ctx_size)for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd)
+        self.config=config
+        self.toks_embd = nn.Embedding(config.vocab_size, config.n_embd)
+        self.pos_embd = nn.Embedding(config.vocab_size, config.n_embd)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+        self.blocks = nn.Sequential(*[Block(config)for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(config.n_embd)
 
     def forward(self, x, targets=None):
         # x should be of shape (batch_size, context_size)
@@ -101,7 +109,7 @@ class Transformer (nn.Module):
 
     def generate(self, idx, max_token_len=100):
         for i in range(max_token_len):
-            idx_cond = idx[:,-self.ctx_size:]
+            idx_cond = idx[:,-self.config.block_size:]
             logits, _ = self(idx_cond)
             logits = logits[:,-1,:]
             probs = F.softmax(logits, dim=-1)
--- mygpt/train.py
+++ mygpt/train.py
@@ -2,22 +2,18 @@ import os
 import pickle
 import numpy as np
 import torch
-from model import Transformer
+from model import Transformer, TransformerConfig
 
 
 # HYPERPARAMETERS
 learning_rate = 1e-3
 batch_size = 32
-n_embd = 64
-n_heads = 8
-context_size = 16 
-n_layer = 6
-max_iter = 10000
+max_iter = 5000
 eval_iter = 100
 eval_interval = 500
 dataset = 'homer'
 device = 'cuda'
-
+config = TransformerConfig()
 load_path = os.path.join('data',dataset)
 #Loading dataset
 train_idxs = np.memmap(os.path.join(load_path, 'train.bin'), dtype=np.uint16, mode='r')
@@ -31,10 +27,11 @@ stoi, itos, vocab_size = ds_meta["stoi"], ds_meta["itos"], ds_meta["vocab_size"]
 
 #Prepare dataset
 def get_batch(split):
+    block_size = config.block_size
     data=train_idxs if split == "train" else eval_idxs
-    ix = torch.randint(len(data) - context_size, (batch_size, ))
-    x =  torch.stack([torch.from_numpy(data[i:i+context_size].astype(np.int64)) for i in ix])
-    y =  torch.stack([torch.from_numpy(data[i+1:i+context_size+1].astype(np.int64)) for i in ix])
+    ix = torch.randint(len(data) - block_size, (batch_size, ))
+    x =  torch.stack([torch.from_numpy(data[i:i+block_size].astype(np.int64)) for i in ix])
+    y =  torch.stack([torch.from_numpy(data[i+1:i+block_size+1].astype(np.int64)) for i in ix])
     x,y = x.to(device),y.to(device)
     return x,y
 
@@ -46,8 +43,6 @@ def estimate_loss():
         losses = torch.zeros(eval_iter)
         for i in range(eval_iter):
             x, y = get_batch(split)
-            B,T = x.shape
-            assert B == batch_size and T==context_size, f"batch is {B} and context is {T} from eval"
             logits, loss = m(x,y)
             losses[i] = loss.item()
         out[split]=losses.mean()
@@ -57,13 +52,14 @@ def estimate_loss():
 
 
 # optimizer
-m = Transformer(vocab_size=vocab_size, ctx_size=context_size, n_embd=n_embd, n_head=n_heads, n_layer=n_layer)
+m = Transformer(config)
 m.to(device)
 adam = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+import time
+start_time = time.time()
 for i in range(max_iter):
     x,y=get_batch('train')
     B,T = x.shape
-    assert B == batch_size and T==context_size, f"batch is {B} and context is {T} from training"
     logits, loss = m(x,y)
     if i%eval_interval==0:
         out = estimate_loss()
@@ -71,8 +67,8 @@ for i in range(max_iter):
     loss.backward()
     adam.step()
     adam.zero_grad(set_to_none=True)
-
-
-
+end_time = time.time()
+print(f"Execution time: {end_time - start_time:.2f} seconds")
+print(f"Each loop on avg time: {(end_time - start_time)/max_iter:.2f} seconds")
 print("".join(itos[i.item()] for i in m.generate(torch.ones((1,1), dtype=torch.long, device=device))[0]))
 
