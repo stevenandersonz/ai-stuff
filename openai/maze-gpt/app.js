fc28fec7c07b2ce7dcd83aafec0a8add5b4b669c
Steven Anderson <sdandersonz@icloud.com>
2023-03-26 00:26:48 -0400

    maze gpt3.5

---
+++ openai/maze-gpt/app.js
@@ -0,0 +1,91 @@
+(function (global) {
+  const Maze = function (rows = 4, cols = 4) {
+    this.rows = rows;
+    this.cols = cols;
+    this.grid = [];
+  };
+  Maze.prototype = {
+    createGrid: function () {
+      console.log(this);
+      for (let i = 0; i < this.rows; i++) {
+        this.grid.push([]);
+        for (let j = 0; j < this.cols; j++) {
+          this.grid[i].push("-"); // Create empty spaces inside thegrid
+        }
+      }
+      this.grid[0][0] = "X";
+      this.grid[this.rows - 1][this.cols - 1] = "O";
+    },
+    // Initialize maze
+    renderUI: function () {
+      const mazeContainer = document.getElementById("maze-container");
+      mazeContainer.innerHTML = "";
+      const root = document.documentElement;
+      root.style.setProperty("--maze-rows", this.rows);
+      root.style.setProperty("--maze-columns", this.cols);
+      for (var i = 0; i < this.rows; i++) {
+        for (var j = 0; j < this.cols; j++) {
+          var cell = document.createElement("div");
+          cell.className = "cell";
+          cell.id = i + "-" + j;
+          document.getElementById("maze-container").appendChild(cell);
+        }
+      }
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          cell = document.getElementById(`${i}-${j}`);
+          if (this.grid[i][j] === "X") {
+            cell.classList.add("entry");
+          } else if (this.grid[i][j] === "O") {
+            cell.classList.add("exit");
+          } else if (this.grid[i][j] === "P") {
+            cell.classList.add("path");
+          }
+          mazeContainer.appendChild(cell);
+        }
+      }
+    },
+    getGrid: function () {
+      return this.grid;
+    },
+    setRows: function (rows) {
+      this.rows = rows;
+    },
+    setCols: function (cols) {
+      this.cols = cols;
+    },
+    setPath: function (path) {
+      for (let [x, y] of path) {
+        this.grid[x][y] = "P";
+      }
+    },
+  };
+  global.Maze = Maze;
+  // Render maze
+})(window);
+
+window.addEventListener("load", () => {
+  let maze = new Maze();
+  maze.createGrid();
+  maze.renderUI();
+  const findPathBtn = document.getElementById("submit");
+  const renderBtn = document.getElementById("render-btn");
+  renderBtn.addEventListener("click", () => {
+    let rows = Number(document.getElementById("rows-config").value);
+    let cols = Number(document.getElementById("cols-config").value);
+    maze = new Maze(rows, cols);
+    maze.createGrid();
+    maze.renderUI();
+  });
+  findPathBtn.addEventListener("click", async () => {
+    const res = await fetch("http://localhost:8080/", {
+      method: "POST",
+      body: JSON.stringify({ board: maze.getGrid() }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => res.json());
+    maze.setPath(res.path);
+    maze.renderUI();
+  });
+});
---
+++ openai/maze-gpt/server/server.py
@@ -0,0 +1,66 @@
+from bottle import Bottle, request, response
+import openai
+import os
+import json
+
+openai.api_key = os.getenv("OPENAPI_KEY") 
+
+def prompt(board, model='gpt-3.5-turbo'):
+    '''
+    using gpt-3.5-turbo model costs $0.002/ 1k tokens.
+    prompt has ~400 tokens so 3 request = ~1200 toks cost ~$0.002 
+
+    using text-davinci-003 model costs $0.02/1k tokens.
+    prompt has ~400 tokens so 3 request = ~1200 toks cost ~$0.02
+    '''
+    prompt = '''
+    You are playing a game, the game consist of the following rules:
+    You are in a {} grid, each cell can have 3 different states: 'X', 'O', or '-' 
+    There is only one cell marked as X, this is the entry point where you start playing.
+    There is only one cell marked as 0, this is the destination where you must end playing.
+    cell marked as - are empty spaces, where you can freely move. However, you can only move right and down.
+    Your objective is to reach the destination cell in the least amount of moves possible.
+
+    {} 
+
+    You must only respond with an valid json object that have the following schema: 
+
+    {{
+        "path":  [[row, col]]
+    }}
+
+    The only key in the object will be "path" which value is a list of tuple, each tuple represents a move, the first element of the tuple is the row and the second element is the column.
+    the path must not include the entry point, and must not include the destination point.
+    Avoid appending any extra key into the JSON object, otherwise the game will not work.
+    Do not under any circumtances reponse with anything else than the JSON object with the "path" key.
+    Avoid replying with an explanation at all cost. 
+    The grid is 0 indexed.
+
+    '''.format(board, f"{len(board)}X{len(board[0])}")
+    if model == 'text-davinci-003':
+        ret = openai.Completion.create(model=model, prompt=prompt, temperature=0, max_tokens=1000)
+        return json.loads(ret.choices[0].text)
+    ret = openai.ChatCompletion.create(model=model,
+    messages=[
+        {"role": "system", "content":"You are an expert maze solver" },
+        {"role": "user", "content": prompt},
+    ])
+    print(ret)
+    return json.loads(ret.choices[0].message.content)
+
+app = Bottle()
+@app.route('/', method=['OPTIONS','POST'])
+def make_move():
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5500'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    if request.method == 'OPTIONS':
+        return {}
+    data = request.json
+    board = data['board']
+    res = prompt(board)
+    return {'path':res['path']} 
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080, debug=True)
